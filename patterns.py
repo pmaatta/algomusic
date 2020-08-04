d7706e4fb5edb419e43f2bb005faf15707c9b19f
pmaatta <petri.maatta@aalto.fi>
2020-08-04 22:54:01 +0300

    added ChordProgression

--- patterns.py
+++ patterns.py
@@ -1,9 +1,13 @@
 import random
+import warnings
 from numpy import exp
 from scipy.stats import norm
 from abc import ABC, abstractmethod
 
 
+# TODO: drum parts straighter (+ argparse option)
+# TODO: rhythm density
+# TODO: make volume limiting internal
 # TODO: percussionsingle
 # TODO: better durations
 # TODO: class "Song" for song generation handling
@@ -44,6 +48,7 @@ class Scale:
         key_name: str
         scale_type: list[int]
         mode: list[int]
+        mode_idx: int
         mode_name: str
         names: list[str]
         all_scale_notes: list[int]
@@ -89,6 +94,7 @@ class Scale:
             mode_idx = random.randint(0, len(scale_type) - 1)
         mode = sorted([(note - scale_type[mode_idx]) % 12 for note in scale_type])
         self.mode = mode
+        self.mode_idx = mode_idx
         if scale_type_name == 'major':
             self.mode_name = Scale.major_mode_names[mode_idx]
         else:
@@ -102,6 +108,117 @@ class Scale:
             self.all_scale_notes = [i for i in range(23, 97) if (i % 12) in scale]
         else:
             self.all_scale_notes = [i for i in range(128) if (i % 12) in scale]
+
+
+class ChordProgression:
+    """
+    Generate a chord progression based on a given scale. 
+    
+    All the chords contain only notes in the scale. If the parameter 'voicing' is 
+    given, all the chords have that particular voicing. Intervals other than the 
+    first and fifth are filtered out of low notes.
+
+    # TODO: update comments
+
+    scale: Scale              Scale of the progression.
+
+    length: int               Length of the progression (number of chords).
+
+    scale_length: int         Number of notes in the scale.
+
+    scale_degrees: list(int)  List of the scale degrees (root notes) of the progression.
+
+    voicing: str/list(int)    If given, determines the structure of all chords in the progression,
+                              in zero-based scale degrees. Must be a key or value in basic_voicings or
+                              a list of ints in range(scale_length). If not given, all chords have a
+                              separate randomly chosen voicing.
+
+    voicing_list: list(list(int))  List of voicings for each chord in the progression.
+
+    chord_progression_notes: list(list(int))  A list containing a list of allowed notes for each chord.
+
+    allow_outside: bool       Whether to allow chords in the progression that are based on notes 
+                              outside of the given scale.
+
+    """
+    
+    basic_voicings = {
+        'triad':    [0, 2, 4],
+        'seventh':  [0, 2, 4, 6],
+        'ninth':    [0, 2, 4, 6, 8],
+        'sixth':    [0, 2, 4, 5],
+        'sus4':     [0, 3, 4],
+        'sus2':     [0, 1, 4],
+        '7sus4':    [0, 3, 4, 6],
+        'add9':     [0, 2, 4, 8],
+        'fifth':    [0, 4],
+        '7no3':     [0, 4, 6]
+    }
+
+    def __init__(self, scale, length=8, repeat=4, voicing=None, allow_outside=None):
+
+        if scale.scale_type_name != 'major':
+            raise NotImplementedError
+            # warnings.warn('voicings do not correspond to scale degrees properly with non-diatonic scales')
+        if scale.mode_idx != 0:
+            warnings.warn('if mode is not ionian the scale degree weighting is off')
+        
+        self.scale = scale
+        self.length = length
+        self.repeat = repeat
+        self.total_length = length * repeat
+        self.scale_length = len(self.scale.scale_type)
+        self.scale_degrees = random.choices(range(self.scale_length), 
+                                            weights=[5,5,5,5,5,5,0],
+                                            k=length)
+        self.voicing_list = None
+        self.chord_progression_notes = None
+
+        if allow_outside is not None:
+            raise NotImplementedError
+        # self.allow_outside = allow_outside if allow_outside is not None else random.choice([True, False])
+
+        if voicing is None: 
+            self.voicing_list = [random.choice(list(self.basic_voicings.values())) for _ in range(self.length)]
+        else:
+            if voicing in self.basic_voicings:
+                voicing = self.basic_voicings[voicing]
+            elif voicing in list(self.basic_voicings.values()):
+                pass
+            else:
+                assert all([isinstance(note, int) for note in voicing]), 'bad value for voicing'
+                assert all([(note in range(self.scale_length)) for note in voicing]), 'bad value for voicing'
+            self.voicing_list = [voicing for _ in range(self.length)]
+
+        chord_progression_notes = []
+
+        for scale_degree, current_voicing in zip(self.scale_degrees, self.voicing_list):
+            voicings = current_voicing * 8
+            all_chord_idxs = []
+            for octave in range(8):
+                all_chord_idxs.extend([note + self.scale_length*octave for note in current_voicing])
+
+            scale_idxs = range(len(scale.all_scale_notes))
+            scale_degree_note = (scale.key + scale.mode[scale_degree]) % 12
+            scale_degree_first_idx = [x % 12 for x in scale.all_scale_notes].index(scale_degree_note)
+            offset_idxs = [i - scale_degree_first_idx for i in scale_idxs]
+            offset_chord_idxs = [offset_idxs.index(note) for note in all_chord_idxs if note in offset_idxs]
+
+            all_chord_notes = []
+            for i, idx in enumerate(offset_chord_idxs):
+                note = scale.all_scale_notes[idx]
+                if note < 40:
+                    if voicings[i] == 0:
+                        all_chord_notes.append(note)
+                elif note < 50:
+                    if voicings[i] in [0, 4]:
+                        all_chord_notes.append(note)
+                else:
+                    all_chord_notes.append(note)
+
+            chord_progression_notes.append(all_chord_notes)
+
+        self.chord_progression_notes = chord_progression_notes * repeat
 
 
 class Pattern(ABC):
@@ -365,7 +482,7 @@ class Snare(Pattern):
         w = []
         for i in range(self.length):
             if i % 8 == 4:
-                w.append(10)
+                w.append(16)
             else:
                 w.append(1)
         start_times = sorted(random.choices(range(self.length), k=self.note_amount, weights=w))
